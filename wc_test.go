fab8399502d4152f372c8af3c669b20502edad2d
chubi-x <chubixax@protonmail.com>
2024-11-25 23:57:09 +0000

    update unit tests

--- wc_test.go
+++ wc_test.go
@@ -5,32 +5,33 @@ package main
 
 import (
 	"bytes"
-	"strings"
 	"testing"
 )
 
 func TestCountBytes(t *testing.T) {
-	reader := bytes.NewReader(make([]byte, 10))
-	readerSize := reader.Size()
-	count := ByteCounter(reader)
 
-	if count != int(readerSize) {
-		t.Errorf("Expected byte count %d, got count %d", readerSize, count)
+	buf := bytes.NewBuffer(make([]byte, 5))
+
+	count := Counter(buf, "bytes")
+	bufLen := buf.Len()
+	if count != bufLen {
+		t.Errorf("Expected byte count %d, got count %d", bufLen, count)
 	}
 }
 func TestCountLines(t *testing.T) {
-	reader := strings.NewReader("This is one line \n this is another line \n this is a third line")
+
+	buf := bytes.NewBuffer([]byte("This is one line \n this is another line \n this is a third line"))
 	lineCount := 3
-	count := LineCounter(reader)
+	count := Counter(buf, "lines")
 
 	if count != int(lineCount) {
 		t.Errorf("Expected line count %d, got count %d", lineCount, count)
 	}
 }
 func TestCountWords(t *testing.T) {
-	reader := strings.NewReader("There are four words")
+	buf := bytes.NewBuffer([]byte("There are four words"))
 	wordCount := 4
-	count := WordCounter(reader)
+	count := Counter(buf, "words")
 
 	if count != int(wordCount) {
 		t.Errorf("Expected word count %d, got count %d", wordCount, count)
